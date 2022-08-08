024274c3ec34a6c1a82d32d37a9e313c71437c5f
paul schauss <p+github@schauss.fr>
2022-08-08 19:18:51 +0200

    Add read & create for shop

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,33 @@
+class ShopsController < ApplicationController
+  before_action :set_shop, only: [:show, :update]
+
+  def index
+    @shops = Shop.all
+  end
+
+  def show
+  end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to shops_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
+
+  def shop_params
+    params.require(:shop).permit(:name)
+  end
+end
