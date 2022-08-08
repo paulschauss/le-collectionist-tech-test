f8414eba8e6c6fec41f7a4fe23d5080da1d66870
paul schauss <p+github@schauss.fr>
2022-08-08 19:22:54 +0200

    Add shop seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+p "Destroy database"
+p "Destroy all shops"
+Shop.destroy_all
+
+p "Seeding database..."
+
+p "Creating shops..."
+shop1 = Shop.create(name: "Shop 1")
+shop2 = Shop.create(name: "Shop 2")
+shop3 = Shop.create(name: "Shop 3")
+shop4 = Shop.create(name: "Shop 4")
+shop5 = Shop.create(name: "Shop 5")
+p "Created #{Shop.count} shops"
