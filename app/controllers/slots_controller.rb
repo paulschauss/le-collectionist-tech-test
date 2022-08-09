e9461bbd89fa151d506c3da908432738ab46c8cf
paul schauss <p+github@schauss.fr>
2022-08-09 11:04:23 +0200

    wip

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,12 +1,24 @@
 class SlotsController < ApplicationController
+  before_action :set_shop, only: %i[new create]
+
   def new
-    @shop = Shop.find(params[:shop_id])
     @slot = Slot.new
   end
 
   def create
-    @shop = Shop.find(params[:shop_id])
-    @slot = @shop.slots.create(slot_params)
+    @slot = Slot.new(slot_params)
+    @slot.shop = @shop
+    @slot.save
     redirect_to shop_path(@shop)
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
+  def slot_params
+    params.require(:slot).permit(:day, :start, :end)
   end
 end
