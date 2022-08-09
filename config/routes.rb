56baafea8691c92150516fa97f6601f33c2750f5
Paul Schauss <95611329+paulschauss@users.noreply.github.com>
2022-08-09 15:44:57 +0200

    [FEAT] Add i18n (#12)
    https://github.com/paulschauss/le-collectionist-tech-test/pull/12

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'shops#index'
-  resources :shops, only: [:index, :show, :new, :create] do
-    resources :slots, only: [:new, :create]
+  scope '(:locale)', locale: /fr|en/ do
+    root 'shops#index'
+    resources :shops, only: [:index, :show, :new, :create] do
+      resources :slots, only: [:new, :create]
+    end
   end
 end
