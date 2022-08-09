494ffce35a2cf745381df6e8d543afc6370372ec
paul schauss <p+github@schauss.fr>
2022-08-09 19:57:02 +0200

    [FIX] Fix slot new form

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,13 +1,13 @@
 class Slot < ApplicationRecord
   AUTORIZED_DAYS = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday].freeze
   AUTORIZED_HOURS = (0..23).freeze
-  AUTORIZE_MINUTES = (0..59).freeze
+  AUTORIZED_MINUTES = (0..59).freeze
 
   belongs_to :shop
 
   validates :day, inclusion: { in: AUTORIZED_DAYS }
   validates :start_hour, :end_hour, inclusion: { in: AUTORIZED_HOURS }
-  validates :start_minute, :end_minute, inclusion: { in: AUTORIZE_MINUTES }
+  validates :start_minute, :end_minute, inclusion: { in: AUTORIZED_MINUTES }
   validates :shop, presence: true
 
   validate :start_hour_is_before_end_hour
