20595b316afb80db6dbe3e4db2a64678b2f2b778
paul schauss <p+github@schauss.fr>
2022-08-10 12:42:06 +0200

    wip

--- app/helpers/slot_helper.rb
+++ app/helpers/slot_helper.rb
@@ -1,7 +1,7 @@
 module SlotHelper
   def display_slot(slot)
     content_tag(:span, class: "slot") do
-      "#{sprintf '%02d', slot.start_hour}h#{sprintf '%02d', slot.start_minute} to #{sprintf '%02d', slot.end_hour}h#{sprintf '%02d', slot.end_minute}"
+      "#{sprintf '%02d', slot.start_hour}:#{sprintf '%02d', slot.start_minute}-#{sprintf '%02d', slot.end_hour}h#{sprintf '%02d', slot.end_minute}"
     end
   end
 end
